9a5b251fefb2dc323f4cbb0ac91d4a19ea53ee98
Gopala <gopalapatel1234@gmail.com>
2023-06-28 14:52:58 +0530

    mas no-unused-vars produce a warning

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 import './style.scss'
 import Board from './components/board'
 function App() {
+  const [counter,setCounter]=useState(1);
+  const onBtnClick=()=>{
+    setCounter(counter+1);
+  };
   return (
     <div className='app'>
-      <Board/>
+      <div>
+        <button onClick={onBtnClick}>Click Me Please</button>
+        <div>{counter}</div>
+      </div>
     </div>
   )
 }
